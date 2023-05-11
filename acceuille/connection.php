a8c3cb9bd70fe4c4d41e54285206837f3d9782ff
Utruna <59669306+Utruna@users.noreply.github.com>
2023-05-11 11:21:22 +0200

    correction de la duplication dans co et crea compt

--- acceuille/connection.php
+++ acceuille/connection.php
@@ -15,23 +15,6 @@ $pdo = new PDO('mysql:host=localhost;dbname=galactique2', 'root', '');
 		$rep = $pdo->prepare('SELECT * FROM joueur WHERE email = :email');
 		$rep->execute(['email' => $email]);
 		$joueur = $rep->fetch();
-<?php
-if (!isset($_SESSION)) {
-	session_start();
-}
-$pdo = new PDO('mysql:host=localhost;dbname=galactique2', 'root', '');
-	if (isset($_POST['email']) && isset($_POST['password'])) {
-		// Récupération des données du formulaire
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-		$password = hash("sha512", $password);
-		$idUnivers = $_POST['idUnivers'];
-		$_SESSION['idUnivers'] = $idUnivers;
-
-		// Vérification si l'email existe dans la base de données
-		$rep = $pdo->prepare('SELECT * FROM joueur WHERE email = :email');
-		$rep->execute(['email' => $email]);
-		$joueur = $rep->fetch();
 
 		if ($joueur && $password == $joueur['password']) {
 			// Le joueur a été trouvé et le mot de passe est correct
--- acceuille/creationCompte.php
+++ acceuille/creationCompte.php
@@ -28,36 +28,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email']) && isset($_P
         $joueur = $envoi->fetch();
         header('Location:index.php');
     }
-<?php
-if (!isset($_SESSION)) {
-	session_start();
-}
-$pdo = new PDO('mysql:host=localhost;dbname=galactique2', 'root', '');
-// Vérification si le formulaire de création de compte a été soumis
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['nom'])) {
-    // Récupération des données du formulaire de création de compte
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $nom = $_POST['nom'];
-    $passwordHacher = hash("sha512", $password);
-
-    // Vérification si unicité du login (email)
-    $rep = $pdo->prepare('SELECT * FROM joueur WHERE email = :email');
-    $rep->execute(['email' => $email]);
-    $joueur = $rep->fetch();
-
-    if ($joueur) {
-        // renvoie sur une page d'erreur hTTP 409
-        header("Location:/erreur409.php");
-    }
-    else {
-        // Envoie dans la bdd
-        $envoi = $pdo->prepare('INSERT INTO joueur (nom, email, password) VALUES (:nom, :email, :password)');
-        $envoi->execute(['nom' => $nom, 'email' => $email, 'password' => $passwordHacher]);
-        $joueur = $envoi->fetch();
-        header('Location:index.php');
-    }
 
 }
 ?>
