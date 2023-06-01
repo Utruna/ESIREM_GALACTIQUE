5a76a3e2ec4b33f7a7c382b247a5326b737d677b
LanclumeC <120129608+LanclumeC@users.noreply.github.com>
2023-06-01 17:55:11 +0200

    Ajout CSS + image et modif HTML page manager

--- galaxie/manager.php
+++ galaxie/manager.php
@@ -46,39 +46,53 @@ $planete = $stmt->fetch(PDO::FETCH_ASSOC);
 <head>
     <meta charset="utf-8" />
     <title>Page de gestion des planetes</title>
-    <link rel="stylesheet" href="../style/css_index.css" />
+    <link rel="stylesheet" href="../style/css_manager.css" />
     <link rel="stylesheet" href="../style/alert.css" />
 </head>
 
 <body>
-<h2>Vous êtes sur la Planete : <?php echo $planete['nom']; ?></h2>
-modification nom de la planete :
-<form action="./../galaxie/modif.php" method="post">
-    <input type="text" name="nomPlanete" value="<?php echo $planete['nom']; ?>">
-    <input type="hidden" name="idPlanete" value="<?php echo $idPlanete; ?>">
-    <button type="submit">Modifier</button>
-</form>
-
-    <form action="./../recherche/recherche.php" method="post">
-        <input type="hidden" name="idPlanete" value="<?php echo $idPlanete; ?>">
-            <button type="submit">Rechercher</button>
-    </form>
-    <form action="./../ChantierSpatial/chantier.php" method="post">
-        <input type="hidden" name="idPlanete" value="<?php echo $idPlanete; ?>">
-            <button type="submit">Chantier Spatial</button>
-    </form>
-    <form action="./../infrastructure/infrastructure.php" method="post">
-        <input type="hidden" name="idPlanete" value="<?php echo $idPlanete; ?>">
-            <button type="submit">Infrastructure</button>
-    </form>
-    <form action="./../defense/defense.php" method="post">
-        <input type="hidden" name="idPlanete" value="<?php echo $idPlanete; ?>">
-            <button type="submit">Defense</button>
-    </form>
+    <img src="../img/arriere_plan_planete.jpg" class="arriere_plan">
+    <h1>Vous êtes sur la Planète : <?php echo $planete['nom']; ?></h1>
+    <div class="objet">
+        <p>Modification du nom de la planète :</p>
+        <form action="./../galaxie/modif.php" method="post">
+            <input type="text" name="nomPlanete" value="<?php echo $planete['nom']; ?>">
+            <input type="hidden" name="idPlanete" value="<?php echo $idPlanete; ?>">
+            <button class="bouton" type="submit">Modifier</button>
+        </form>
+    </div>
+    <div class="objet_1">
+        <img src="../img/symbole_science.png" alt="Symbole de Recherche"/>
+        <form action="./../recherche/recherche.php" method="post">
+            <input type="hidden" name="idPlanete" value="<?php echo $idPlanete; ?>">
+            <button class="bouton" type="submit">Aller au laboratoire de recherche</button>
+        </form>
+    </div>
+    <div class="objet_2">
+        <img src="../img/symbole_chantier-spatial.png" alt="Symbole de chantier Spatial"/>
+        <form action="./../ChantierSpatial/chantier.php" method="post">
+            <input type="hidden" name="idPlanete" value="<?php echo $idPlanete; ?>">
+            <button class="bouton" type="submit">Aller au chantier spatial</button>
+        </form>
+    </div>
+    <div class="objet_3">
+        <img src="../img/symbole_infrastructure.png" alt="Symbole d'infrastructure"/>
+        <form action="./../infrastructure/infrastructure.php" method="post">
+            <input type="hidden" name="idPlanete" value="<?php echo $idPlanete; ?>">
+            <button class="bouton" type="submit">Aller aux infrastructures</button>
+        </form>
+    </div>
+    <div class="objet_4">
+        <img src="../img/symbole_defense.png" alt="Symbole de defense"/>
+        <form action="./../defense/defense.php" method="post">
+            <input type="hidden" name="idPlanete" value="<?php echo $idPlanete; ?>">
+            <button class="bouton" type="submit">Aller aux systèmes de defense</button>
+        </form>
+    </div>
     <form method="post" action="./galaxie.php">
-        <button type="submit">Retour</button>
+        <button class="retour" type="submit">Retour</button>
     </form>
-    <div>
+    <div id="div_ressources">
         <h2>Ressource Joueur</h2>
         <p class="resource">Métal : <?php echo $ressource['metal'] ?></p>
         <p class="resource">Energie : <?php echo $ressource['energie'] ?></p>
